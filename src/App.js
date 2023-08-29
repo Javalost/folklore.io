c858b22de490e1f41456ce993984666cdb77fb7f
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2023-08-29 00:31:17 -0500

    Boilerplate testing files deleted
     -Redundancies deleted from stripped down project

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import {
   Routes
 } from 'react-router-dom';
 import './App.css';
-import DataTest from './Components/DataTest';
-import FormTest from './Components/FormTest'; 
+
 import Landing from './Components/LandingPage/Landing';
 import CombinedMapContent from './Components/MapBoxContent/CombinedMapContent'
 
@@ -15,8 +14,6 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/datatest" element={<DataTest />} />
-          <Route path="/formtest" element={<FormTest />} /> 
           <Route path="/" element={<Landing/>} />
           <Route path="/mapbox" element={<CombinedMapContent/>}/>
         </Routes>
--- src/Components/MapBoxContent/MapBox.js
+++ src/Components/MapBoxContent/MapBox.js
@@ -1,26 +1,35 @@
-import Map from 'react-map-gl';
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 function MapBox() {
-  const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN;
+    const mapContainerRef = useRef(null);
+    const mapboxToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-  if (!mapboxToken) {
-    return <div>Error: Mapbox token not found!</div>;
-  }
+    useEffect(() => {
+        if (!mapboxToken) {
+            console.error("Error: Mapbox token not found!");
+            return;
+        }
 
-  return (
-    <>
-      <Map
-        mapboxAccessToken={mapboxToken}
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14
-        }}
-        style={{width: '100%', height: '100%'}}
-        mapStyle="mapbox://styles/folkiomap/cllu40olo00xn01qrh53b9u3i"
-      />
-    </>
-  );
+        mapboxgl.accessToken = mapboxToken;
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/folkiomap/cllu40olo00xn01qrh53b9u3i',
+            center: [-122.4, 37.8], 
+            zoom: 14
+        });
+
+        // Cleanup on unmount
+        return () => {
+            map.remove();
+        };
+    }, [mapboxToken]); // The dependency array ensures this useEffect runs once and if the token changes.
+
+    if (!mapboxToken) {
+        return <div>Error: Mapbox token not found!</div>;
+    }
+
+    return <div ref={mapContainerRef} style={{ width: '100%', height: '100%' }} />;
 }
 
 export default MapBox;
