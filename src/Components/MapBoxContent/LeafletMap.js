d0a0dd15d4fe4b2aa7c8f5de964f250972c7c7b1
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2023-08-31 00:41:45 -0500

    previous-change

--- src/Components/MapBoxContent/LeafletMap.js
+++ src/Components/MapBoxContent/LeafletMap.js
@@ -3,11 +3,18 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';  // Important: This imports the default styles
 import L from 'leaflet';  // Import the leaflet library 
 import { Typography, Avatar } from '@mui/material';
-import LocationOnIcon from '@mui/icons-material/LocationOn';  // Import the icon
+//import LocationOnIcon from '@mui/icons-material/LocationOn';  // Import the icon
+
+// Set the bounds to restrict the map view
+const WORLD_BOUNDS = [
+    [-85, -180],  // Southwest coordinates
+    [85, 180]     // Northeast coordinates
+];
+
+
+
 
 // Get the SVG string of the LocationOn icon and convert it to data URI format
-const svgIcon = <LocationOnIcon style={{ fontSize: 40, color: 'red' }} />; 
-console.log(svgIcon);
 
 const svgPath = `<path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>`;
 
@@ -16,8 +23,6 @@ const svgUrl = encodeURIComponent(`
     ${svgPath}
   </svg>
 `);
- 
-
 
 // Define the custom icon
 const customIcon = new L.Icon({
@@ -25,14 +30,45 @@ const customIcon = new L.Icon({
   iconSize: [40, 40],
   iconAnchor: [20, 40],
   popupAnchor: [0, -40]
-});
+}); 
+
+function handleMoveEnd(event) {
+    const { lat, lng } = event.target.getCenter();
+    let newLng = lng;
+    
+    while (newLng < -180) {
+        newLng += 360;
+    }
+    
+    while (newLng > 180) {
+        newLng -= 360;
+    }
+    
+    if (lng !== newLng) {
+        event.target.panTo([lat, newLng]);
+    }
+}
+
 
 function LeafletMap({ stories, onMarkerClick }) {
     return (
-        <MapContainer center={[21.8518, -102.2877]} zoom={6} style={{ width: '100%', height: '100vh' }}>
+        <MapContainer 
+            center={[21.8518, -102.2877]} 
+            zoom={8} 
+            style={{ width: '100%', height: '100vh' }}
+            maxBounds={WORLD_BOUNDS}
+            maxZoom={5}  // this line will prevent users from zooming out further than the default zoom level
+        >
+
+
+
+
+
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                noWrap={true} // add this
             />
+
             {stories.map(story => (
                 <Marker 
                   key={story.country} 
@@ -63,7 +99,6 @@ function LeafletMap({ stories, onMarkerClick }) {
                                 alt={`${story.country} flag`}
                                 sx={{ width: 40, height: 40 }}
                             />
-
                         </div>
                         <div style={{ backgroundColor: '#yourColorForStory', padding: '8px 10px', width: '260px' }}>
                             <Typography 
@@ -83,14 +118,8 @@ function LeafletMap({ stories, onMarkerClick }) {
                             </Typography>
                         </div>
                     </Popup>
-
-
-
                 </Marker>
             ))}
-            
-            {/* Test Marker */}
-            
         </MapContainer>
     );
 }
