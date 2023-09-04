93562cf1c97f9712989b3d7dd5140a3095417fb4
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2023-09-04 12:13:15 -0500

    changes

--- src/Components/LandingPage/Landing.js
+++ src/Components/LandingPage/Landing.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import VerticalTabs from './VerticalTabs';
-import MainContent from './MainContent'; 
 import CombinedContent from './CombinedContent';
 
 function Landing() {
-  const [selectedTab, setSelectedTab] = React.useState(0);
 
   return (
     <div style={{ display: 'flex', height: '100vh' }}>
--- src/Components/LandingPage/TabOneContent/ProductTab.js
+++ src/Components/LandingPage/TabOneContent/ProductTab.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography, Tabs, Tab, TextField, Box, Select, MenuItem, Button } from '@mui/material';
+import { Card, CardContent, Typography, Tabs, Tab, TextField, Box, Button } from '@mui/material';
 
 function ProductTab() {
   const [value, setValue] = React.useState(0);
@@ -10,46 +10,34 @@ function ProductTab() {
 
   return (
     <Card variant="outlined" sx={{ width: '27rem', height: '28rem', padding: '30px'}}>
-      <CardContent sx={{ padding: '8px', display: 'flex', flexDirection: 'column', gap: '20px', marginTop: '1.75rem'}}>
+      <CardContent sx={{ padding: '8px', display: 'flex', flexDirection: 'column', height: '100%' }}>
 
-        <Box sx={{ display: 'flex', justifyContent: 'space-between'}}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', }}>
             <Typography variant="h3" gutterBottom>
-                Try It Out!
+                LOG IN
             </Typography>  
-            <Tabs value={value} onChange={handleTabChange} size="small" indicatorColor="primary" sx={{
-        }}>
-          <Tab size ="large" label="T1" />
-          <Tab label="T2" />
-        </Tabs>
-        </Box>
+            <Tabs value={value} onChange={handleTabChange} size="small" indicatorColor="primary">
+              <Tab size="large" label="T1" />
+              <Tab label="T2" />
+            </Tabs>
+          </Box>
 
-        
-        <TextField
-            size="medium"
-            label="Input"
-            variant="outlined"
-            fullWidth
-            sx={{ height: '48px'}}
-            />
-            
+          <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', gap:'3rem', marginTop: '3rem' }}>
             <TextField
-            size="medium"
-            label="Input"
-            variant="outlined"
-            fullWidth
-            sx={{ height: '48px' }}
-        />
-
-        <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: '6px' }}>
-            <Select size="small" variant="outlined" value="" sx={{ flex: 1, marginRight: '6px', height: '48px' }}>
-                <MenuItem value={10}>DROP1</MenuItem>
-                <MenuItem value={20}>DROP2</MenuItem>
-            </Select>
-
-            <Select size="small" variant="outlined" value="" sx={{ flex: 1, marginLeft: '6px', height: '48px' }}>
-                <MenuItem value={10}>DROP1</MenuItem>
-                <MenuItem value={20}>DROP2</MenuItem>
-            </Select>
+                size="medium"
+                label="Username"
+                variant="outlined"
+                fullWidth
+                sx={{ height: '48px'}}
+            />
+                
+            <TextField
+                size="medium"
+                label="Password"
+                variant="outlined"
+                fullWidth
+                sx={{ height: '48px' }}
+            />
         </Box>
 
         <Button 
@@ -58,7 +46,7 @@ function ProductTab() {
             size="small" 
             color="primary" 
             fullWidth 
-            sx={{ marginTop: '0', padding: '4px 8px', height: '4rem' }}>
+            sx={{ padding: '4px 8px', height: '4rem', }}>
           SUBMIT
         </Button>
       </CardContent>
