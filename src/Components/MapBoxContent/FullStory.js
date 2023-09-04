851d53b64dd015fb1222dcb6988066186ab3635c
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2023-09-03 23:02:05 -0500

    feat: Add interactive map, and UI improvements
    - Integrated an interactive Leaflet map to display folklore stories on markers
    - Created components for story display and navigation
    - Improved UI layout for story details, pagination, and drawer
    - Added custom scrollbar styles for better user experience
    - Refactored code to enhance readability and maintainability
    
    Closes #issue_number

--- src/Components/MapBoxContent/FullStory.js
+++ src/Components/MapBoxContent/FullStory.js
@@ -11,7 +11,17 @@ console.log('Inside FullStory - totalStories:', totalStories);
   return (
         <Box sx={{ padding: 2, display: 'flex', flexDirection: 'column', alignItems:'center' }}> 
             <Box sx={{display: 'flex', flexDirection:'row', alignItems:'center', gap: '15px'}}> 
-                <Button sx={{height:'3rem'}} variant="contained" startIcon={<ArrowLeftIcon/>} onClick={() => onSwitchStory(-1)}></Button> 
+                <IconButton 
+                    onClick={() => onSwitchStory(-1)} 
+                    style={{
+                        padding: 8, 
+                        borderRadius: '50%', 
+                        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',  // You can adjust this shadow value as per your needs 
+                        border: '1px solid white'
+                    }}
+                >
+                    <ArrowLeftIcon style={{ fontSize: 24, color: 'white' }} />
+                </IconButton> 
                 <Card elevation={3} sx={{borderRadius: '15px', width: '35rem', height: '35rem', display: 'flex', flexDirection: 'column'}}>
                     <CardHeader
                         sx={{ borderRadius: '14px', background: 'linear-gradient(45deg, #539bfe, #2979ff, #005cd2)'}}
@@ -28,17 +38,21 @@ console.log('Inside FullStory - totalStories:', totalStories);
                         }
                     />
                     <CardContent
-                    sx={{marginTop: '3rem'}}
+                        sx={{marginTop: '3rem',
+                             height: '100vh', 
+                             overflowY:'auto'    
+                    }}
                     >
                         <Typography 
                             variant="body1"
                             color="text.secondary"
                             paragraph
+                            overflowY="auto"
                         >
                             {story.story}
                         </Typography>
                     </CardContent>
-                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent:'flex-end', height: '100vh', padding: 2 }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent:'flex-end', height: '3rem', padding: 2 }}>
                         <Typography>{storyIndex + 1} of {totalStories}</Typography>
                     </Box>
                 </Card>
