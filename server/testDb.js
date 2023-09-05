7ea8cd389b13a50d6d8b233a334f70f46783fcb2
joseb2 <thompsonjose3@gmail.com>
2023-09-04 23:39:01 -0500

    feat: Set up Express server/integrate ElephantSQL
    - Initialized an Express server on port 3000.
    - Integrated with ElephantSQL using `pg` for PostgreSQL connection.
    - Added `/stories` endpoint to fetch stories from the database.
    - Implemented error handling for database operations.

---
+++ server/testDb.js
@@ -0,0 +1,10 @@
+const pool = require('./database');
+
+pool.query('SELECT NOW()', (error, results) => {
+  if (error) {
+    console.error('Error connecting to the database:', error);
+    process.exit(1);
+  }
+  console.log('Connected! Database time:', results.rows[0].now);
+  pool.end();  // Close the pool after testing
+});
