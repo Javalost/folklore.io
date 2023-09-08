9954c38c4f708f6074d812f3304bb6cf79c89788
Jose Avalos-Thompson <thompsonjose3@gmail.com>
2023-09-08 02:03:54 -0500

    Changes made to border
    Changed borders for Toolbar content. Settled on keeping all
    markers be displayed by default.

--- src/Components/MapBoxContent/StoryFilter.js
+++ src/Components/MapBoxContent/StoryFilter.js
@@ -3,73 +3,96 @@ import { FormControl, InputLabel, Typography, Select, Button, OutlinedInput, Men
 import axios from 'axios';
 
 function StoryFilter({ onCountrySelect }) {
-  
+  const [countries, setCountries] = useState([]);
+  const [selectedCountries, setSelectedCountries] = useState([]);
 
-   const [countries, setCountries] = useState([]);
-    const [selectedCountries, setSelectedCountries] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3001/countries')
+      .then(response => {
+        setCountries(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching countries:", error);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:3001/countries')
-            .then(response => {
-                setCountries(response.data);
-            })
-            .catch(error => {
-                console.error("Error fetching countries:", error);
-            });
-    }, []);
+  const handleChange = (event) => {
+    setSelectedCountries(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setSelectedCountries(event.target.value);
-    }; 
+  const handleSubmit = () => {
+    if (typeof onCountrySelect === "function") {
+      onCountrySelect(selectedCountries);  // Pass selected country to callback
+    }
+  }; 
 
-    const handleSubmit = () => {
-      if (typeof onCountrySelect === "function") {
-          onCountrySelect(selectedCountries);  // Pass selected country to callback
-      }
+  const selectedMenuItemStyle = {
+    backgroundColor: '#2979ff', // This is an example color, you can change it.
+    color: 'white', // Color for text
   };
   
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center'}}>
-        <Typography variant="h6" style={{ marginRight: '16px', border: '1px solid', padding: '5px', borderRadius: '15px' }}>
-            FILTER BY COUNTRY
-        </Typography>
-        <FormControl sx={{ m: 1, width: 150 }}>
-            <InputLabel 
-                id="select-country-label" 
-                sx={{border: 'white',
-                    color: 'white',
-                '&.Mui-focused': { 
-                    color: 'white',
-                }}}
-                shrink={true}>
-                Countries
-            </InputLabel>
-            <Select
-                labelId="select-country-label"
-                id="select-country-label"
-                multiple
-                value={selectedCountries}
-                onChange={handleChange}
-                input={<OutlinedInput label="Countries" style={{ height: '36px', color: 'white', boxSizing: 'border-box' }} />}
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <Typography variant="h6" style={{ marginRight: '16px', border: '1px solid', padding: '5px', borderRadius: '15px' }}>
+        FILTER BY COUNTRY
+      </Typography>
+      <FormControl sx={{ m: 1, width: 150 }}>
+        <InputLabel
+          id="select-country-label"
+          sx={{
+            border: 'white',
+            color: 'white',
+            '&.Mui-focused': {
+              color: 'white',
+            },
+          }}
+          shrink={true}>
+          Countries
+        </InputLabel>
+        <Select
+          labelId="select-country-label"
+          id="select-country-label"
+          multiple
+          value={selectedCountries}
+          onChange={handleChange}
+          input={<OutlinedInput label="Countries" style={{ height: '36px', color: 'white', boxSizing: 'border-box' }} />}
+          sx={{
+            '& .Mui-selected': {
+              color: '#f5f5f5', // Change the color of selected items to green
+            }, 
+            '& fieldset': { borderColor: 'white' },
+            '&:hover fieldset': { borderColor: 'white' },
+            '&.Mui-focused fieldset': { borderColor: 'white' },
+          }}
+        >
+          {countries.map((country, index) => (
+            <MenuItem 
+                key={index} 
+                value={country}
+                style={selectedCountries.includes(country) ? selectedMenuItemStyle : {}}
+                >
+              {country}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit}
+        style={{ 
+            height: '36px', 
+            verticalAlign: 'middle', 
+            backgroundColor: '#2664c4', 
+            color: 'white', 
+            borderColor:'white'
+            }}
             >
-                {countries.map((country, index) => (
-                    <MenuItem key={index} value={country}>
-                        {country}
-                    </MenuItem>
-                ))}
-            </Select>
-        </FormControl>   
-        <Button 
-            variant="contained" 
-            color="primary" 
-            onClick={handleSubmit}
-            style={{ height: '36px', verticalAlign: 'middle' }}>  
-            Submit
-        </Button>
-    </div>  
-);
-
+        Submit
+      </Button>
+    </div>
+  );
 }
 
 export default StoryFilter;
